65f91ebfafa599db991d458b2aa4cc12f38d6635
cryptomathematician <bosman006@st.ug.edu.gh>
2024-10-06 19:50:13 +0000

    server side

---
+++ server_time.js
@@ -0,0 +1,12 @@
+require("http").createServer((inRequest, inResponse) => {
+    const requestModule = require("request");
+    requestModule(
+    "http://worldtimeapi.org/api/timezone/America/New_York",
+    function (inErr, inResp, inBody) {
+    inResponse.end(
+    `Hello from my first Node Web server: ${inBody}`
+    );
+    }
+    );
+}).listen(80)
+
